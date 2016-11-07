4ecf22716de8b0260251fbc350151ab2a11c4280
Wayne Tng <wayne@futureworkz.com>
2016-11-07 13:18:53 +0700

    06: create components for orders inventory and header

---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Order extends React.Component {
+  render() {
+    return(
+      <div>
+        this is order
+      </div>
+    )
+  }
+}
+
+export default Order
