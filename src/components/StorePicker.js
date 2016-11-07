230c5af93d50066a279043668e27424149af29f2
Wayne Tng <wayne@futureworkz.com>
2016-11-07 14:54:44 +0700

    11: Arrow function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,12 +3,16 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault()
+  }
+
   render() {
     return (
       <div>
-        <form className='store-selector'>
+        <form className='store-selector' onSubmit={(e) => this.goToStore(e)}>
           <h2> Please enter a store </h2>
-          <input type='text' required={true} placeholder='Store Name' defaultValue={getFunName()} />
+          <input ref={(input) => {this.storeInput = input} } type='text' required={true} placeholder='Store Name' defaultValue={getFunName()} />
           <button type='submit'>
             Visit Store
           </button>
