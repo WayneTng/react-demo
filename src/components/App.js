03979280838dddaa88dcc8d3fce883fdd79fe888
Wayne Tng <wayne@futureworkz.com>
2016-11-07 16:47:41 +0700

    13: State

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,24 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {},
+      inventory: {}
+    }
+  }
+
+  addFish(fish) {
+    const fishes = {...this.state.fishes}
+    const timestamp = Date.now()
+
+    fishes[`fish-${timestamp}`] = fish
+
+    this.setState({ fishes })
+  }
+
   render() {
     return(
       <div className='catch-of-the-day'>
@@ -11,7 +29,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={(fish) => this.addFish(fish)} />
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
   render() {
+    const { addFish } = this.props
+
     return(
       <div>
-        This is inventory
+        <h2> Inventory </h2>
+        <AddFishForm addFish={addFish} />
       </div>
     )
   }
